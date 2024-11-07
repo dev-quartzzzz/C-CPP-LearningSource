1cce0623b429909a45c5d6928997cc858fb21b77
dev-quartzzzz <rain-piyapong@outlook.com>
2024-11-07 13:35:04 +0700

    change

---
+++ extra-pointer-q04-dev-quartzzzz-main/main.c
@@ -0,0 +1,55 @@
+/*
+---------- Pointer Extra: Geometry Calculator -------------
+Student ID: 6788094
+Name: Piyapong
+Surname: Pongruang
+Section: 3
+-----------------------------------------------------------
+*/
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 20
+
+void area_cal(char shape, char *shape_name, float *area) {
+  if (shape == 'S') {
+    // Your code started here.
+
+    
+  } else if (shape == 'R') {
+    // Your code started here.
+
+    
+  } else if (shape == 'C') {
+    // Your code started here.
+    
+    
+  } else if (shape == 'T') {
+    // Your code started here.
+    
+    
+  } else if (shape == 'Z') {
+    // Your code started here.
+    
+    
+  }
+}
+
+int main(void) {
+  char shape, shape_name[MAX];
+  float area;
+  scanf(" %c", &shape);
+  if (shape == 'S' || shape == 'R' || shape == 'C' || shape == 'T' ||
+      shape == 'Z') {
+    // Your code started here.
+    
+    
+  } else {
+    // Your code started here.
+    
+    
+  }
+  return 0;
+}
